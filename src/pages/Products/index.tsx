9a52b5646b1a45c90abee948aa2296fe5995447b
PhanThanhDat16 <phandat1662004@gmail.com>
2025-04-23 21:54:01 +0700

    fix: fix UI develop product

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -72,7 +72,6 @@ const Product = () => {
     { title: 'Price', dataKey: 'price' },
     { title: 'Old Price', dataKey: 'oldprice' },
     { title: 'Category', dataKey: 'category' },
-    { title: 'Image', dataKey: 'image' },
     {
       title: 'Actions',
       dataKey: 'actions',
@@ -96,14 +95,13 @@ const Product = () => {
     price: product.price,
     oldprice: product.oldprice,
     category: product.category,
-    image: product.image,
     stock: product.stock,
     actions: product
   }))
 
   return (
     <>
-      <h2 className="text-2xl font-semibold mt-6">Product</h2>
+      <h2 className="mt-6 text-2xl font-semibold">Product</h2>
       {/* export */}
       <div className="grid w-full grid-cols-2 gap-4 p-4 mt-6 text-sm bg-white rounded-md">
         <ExportImport />
@@ -131,14 +129,14 @@ const Product = () => {
           className="items-center h-12 px-4 my-2 bg-gray-100 border rounded-md"
         />
         <div className="">
-          <select className="items-center h-12 px-2 my-2 bg-gray-100 border rounded-md w-full">
+          <select className="items-center w-full h-12 px-2 my-2 bg-gray-100 border rounded-md">
             <option value="">Select Category</option>
             <option value="1">Category 1</option>
             <option value="2">Category 2</option>
           </select>
         </div>
         <div>
-          <select className="items-center h-12 px-4 my-2 bg-gray-100 border rounded-md w-full">
+          <select className="items-center w-full h-12 px-4 my-2 bg-gray-100 border rounded-md">
             <option value="">Select Orderby</option>
             <option value="1">Orderby 1</option>
             <option value="2">Orderby 2</option>
@@ -153,8 +151,57 @@ const Product = () => {
       </div>
 
       {/* table */}
-      <div className="overflow-x-auto mt-6">
-        <Table columns={columns} data={dataFormat as any} />
+      <div className="mt-4 overflow-hidden bg-white rounded-lg">
+        <div className="w-full overflow-hidden overflow-x-scroll">
+          <Table columns={columns} data={dataFormat as any} />
+        </div>
+        <div className="flex flex-col justify-between p-4 text-xs text-gray-600 sm:flex-row dark:text-gray-400">
+          <span className="flex items-center font-semibold tracking-wide uppercase">Showing 1-12 of 12</span>
+          <div className="flex mt-2 sm:mt-auto sm:justify-end">
+            <nav aria-label="Table navigation">
+              <ul className="inline-flex items-center">
+                <li>
+                  <button
+                    className="inline-flex items-center justify-center p-2 font-medium leading-5 text-gray-600 align-bottom transition-colors duration-150 border border-transparent rounded-md opacity-50 cursor-pointer focus:outline-none dark:text-gray-400"
+                    type="button"
+                    aria-label="Previous"
+                  >
+                    <svg className="w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                      <path
+                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="inline-flex items-center justify-center px-3 py-1 text-xs font-medium leading-5 text-white align-bottom transition-colors duration-150 border border-transparent rounded-md cursor-pointer focus:outline-none bg-emerald-500 active:bg-emerald-600 hover:bg-emerald-600"
+                    type="button"
+                  >
+                    1
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="inline-flex items-center justify-center p-2 font-medium leading-5 text-gray-600 align-bottom transition-colors duration-150 border border-transparent rounded-md opacity-50 cursor-pointer cursor-not-allowed focus:outline-none dark:text-gray-400"
+                    type="button"
+                    aria-label="Next"
+                  >
+                    <svg className="w-3 h-3" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                      <path
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clipRule="evenodd"
+                        fillRule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
     </>
   )
